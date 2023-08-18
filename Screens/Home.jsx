44bf0e774a1c7ce646347dc62fb1957132fc5d14
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-18 19:08:41 +0600

    chore: add back button to the second screen

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function HelloWorld({navigation}) {
     return (
         <View>
             <Text>Hello World!</Text>
+            <Button title="Go back" onPress={() => navigation.goBack()} />
         </View>
     )
 }
