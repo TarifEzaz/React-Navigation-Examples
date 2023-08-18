c7c7210a17da91c0ba1075f019c84560e39f01d5
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-18 18:40:01 +0600

    feat: use navigation prop passed to the screen components defined inside the stack Navigator component
    Screen components receive the navigation prop if they are wrapped inside the stack Navigator component. This prop can be used to move to other stack Screens defined inside stack Navigators

--- Screens/Welcome.jsx
+++ Screens/Welcome.jsx
@@ -1,9 +1,13 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function WelcomeWorld({navigation}) {
     return (
-        <View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Welcome World!</Text>
+            <Button
+                title="Go to Home Page!"
+                onPress={() => navigation.navigate('Home')}
+            />
         </View>
     )
 }
