6610826b7568b71f4175d45e9e7757a6b1b722ef
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-20 14:57:06 +0600

    feat: add react-native-gesture-handler at the root as per https://stackoverflow.com/questions/67130651/reanimated-2-failed-to-create-a-worklet-maybe-you-forgot-to-add-reanimateds-ba

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HelloWorld from './Screens/Home';
 import WelcomeWorld from './Screens/Welcome';
 import Tab from "./Screens/Tab";
+import Drawer from "./Screens/Drawer";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Tab">
+      <Stack.Navigator initialRouteName="Drawer">
         <Stack.Screen name="Home" component={HelloWorld}/>
         <Stack.Screen name="Welcome" component={WelcomeWorld} />
         <Stack.Screen name="Tab" component={Tab} />
+        <Stack.Screen name="Drawer" component={Drawer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
