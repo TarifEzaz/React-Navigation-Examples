f3994319eb0af6be95d7385004f40c9fbe1542a3
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-20 15:41:48 +0600

    chore: refactor component for demonstrating tab Navigator, stack Navigator and Drawer navigator

--- App.js
+++ App.js
@@ -1,23 +1,21 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HelloWorld from './Screens/Home';
-import WelcomeWorld from './Screens/Welcome';
-import Tab from "./Screens/Tab";
+import Tabs from "./Screens/Tab";
 import Drawer from "./Screens/Drawer";
+import Stack from "./Screens/Stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Tab">
-        <Stack.Screen name="Home" component={HelloWorld} options={{ title: 'My Home' }}/>
-        <Stack.Screen name="Welcome" component={WelcomeWorld} options={{ title: 'My Welcome' }} />
-        <Stack.Screen name="Tab" component={Tab} />
-        <Stack.Screen name="Drawer" component={Drawer} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Stack" component={Stack} options={{ title: 'My Stack' }}/>
+        <Tab.Screen name="Tab" component={Tabs} />
+        <Tab.Screen name="Drawer" component={Drawer} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
