7c3eb7351af4fd74ff865f46c74e9e57b801d5c6
Tarif Ezaz <tarif.ezaz@gmail.com>
2023-08-20 17:55:31 +0600

    feat: add header styles
    headerStyle: a style object that will be applied to the View that wraps the header. If you set backgroundColor on it, that will be the color of your header.
    headerTintColor: the back button and title both use this property as their color. In the example below, we set the tint color to white (#fff) so the back button and the header title would be white.
    headerTitleStyle: if we want to customize the fontFamily, fontWeight and other Text style properties for the title, we can use this to do it.

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -4,10 +4,24 @@ export default function HelloWorld({navigation}) {
     return (
         <View>
             <Text>Hello World!</Text>
-            <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Button 
+                title="Go back" onPress={() => navigation.goBack()} />
             <Button
                 title="Update the title"
-                onPress={() => navigation.setOptions({ title: 'Updated!' })}
+                onPress={
+                    () => navigation.setOptions(
+                        { 
+                            title: 'Updated!',           
+                            headerStyle: {
+                                backgroundColor: '#f4511e',
+                            },
+                            headerTintColor: '#fff',
+                            headerTitleStyle: {
+                                fontWeight: 'bold',
+                            }, 
+                        }
+                    )
+                }                
             />
         </View>
     )
